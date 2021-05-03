106a59184ee52cce639758fbe968081dacf6bb24
Tomasz Pielczyk <tomasz.pielczyk@gmail.com>
2021-05-03 21:52:23 +0200

    refactor command, add query, public event

--- app/Example/test.php
+++ app/Example/test.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Example\RegisterUserCommand;
+use App\Example\UserQuery;
 use App\System\CommandBus;
 use App\System\CommandResult;
 use App\System\QueryBus;
@@ -9,15 +10,10 @@ use App\System\QueryBus;
 $registration = CommandBus::instance()->execute(new RegisterUserCommand('test@example.com', '1234'));
 // vs v2
 (new RegisterUserCommand('test@example.com', '1234'))->execute();
-// vs v3
-RegisterUserCommand::new('test@example.com', '1234')->execute();
 
 /** @var UserModel $user */
-$user = QueryBus::instance()->ask(new UserQuery('1'));
+$response = QueryBus::instance()->ask(new UserQuery('1'));
 // vs v2
-(new UserQuery('1'))->ask();
-// vs v3
-UserQuery::new('1')->ask();
-
-
+$response = (new UserQuery('1'))->ask();
+var_dump($response->user());
 
